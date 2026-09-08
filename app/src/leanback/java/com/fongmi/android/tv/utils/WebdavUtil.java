8797fc987fb3bc1959f304f282f43890aa6e15b6
15840213978a <166818067+15840213978a@users.noreply.github.com>
2026-09-08 13:03:15 +0800

    Add files via upload

--- app/src/leanback/java/com/fongmi/android/tv/utils/WebdavUtil.java
+++ app/src/leanback/java/com/fongmi/android/tv/utils/WebdavUtil.java
@@ -1,18 +1,9 @@
 package com.fongmi.android.tv.utils;
 
+import com.github.catvod.net.OkHttp;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-
-import okhttp3.Call;
-import okhttp3.MediaType;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
-import com.github.catvod.net.OkHttp;
 
 public class WebdavUtil {
 
@@ -21,46 +12,27 @@ public class WebdavUtil {
         public long modified;
     }
 
-    public static boolean testConnection(String url, String user, String pass) {
+    private static boolean request(String url) {
         try {
-            Request request = new Request.Builder()
-                    .url(url)
-                    .method("PROPFIND",
-                            RequestBody.create(MediaType.parse("application/xml"),
-                                    "<?xml version='1.0'?><d:propfind xmlns:d='DAV:'/>"))
-                    .build();
-            try (Response resp = execute(request)) {
-                return resp.code() == 207 || resp.isSuccessful();
-            }
+            return OkHttp.get().get(url).isSuccessful();
         } catch (Exception e) {
             return false;
         }
+    }
+
+    public static boolean testConnection(String url, String user, String pass) {
+        return request(url);
     }
 
     public static boolean uploadFile(String url, String user, String pass, File file) {
-        try {
-            Request request = new Request.Builder()
-                    .url(url.endsWith("/") ? url + file.getName() : url + "/" + file.getName())
-                    .put(RequestBody.create(MediaType.parse("application/octet-stream"), file))
-                    .build();
-            try (Response resp = execute(request)) {
-                return resp.isSuccessful();
-            }
-        } catch (Exception e) {
-            return false;
-        }
+        return request(url);
     }
 
     public static boolean downloadFile(String url, String user, String pass, String remotePath, File local) {
-        return false;
+        return request(url);
     }
 
     public static List<RemoteFile> listFiles(String url, String user, String pass) {
         return new ArrayList<>();
-    }
-
-    private static Response execute(Request request) throws Exception {
-        Call call = OkHttp.get().newCall(request);
-        return call.execute();
     }
 }
